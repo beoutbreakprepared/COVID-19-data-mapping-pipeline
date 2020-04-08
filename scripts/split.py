86ad8559b0bc506945551904ee74328cbca94fe7
Manu Cornet <m@ma.nu>
2020-04-08 20:09:38 +0000

    Update split.py to take full-data.json as an input, and spit out daily slices.

--- scripts/split.py
+++ scripts/split.py
@@ -1,30 +1,30 @@
 import json
+import functions
 import os
 import sys
 
+TEMP_GEOJSON = "temp.geojson"
 TOPLEVEL_KEY = "data"
 
 def normalize_date(date):
-  # Sometimes we get a date range instead of a single date.
-  # Let's get the end of the range.
-  if "-" in date:
-    date = date.split("-")[1]
+  date = date.replace("-", ".")
   if len(date) == len("D.MM.YYYY"):
     # Single day digit
     date = date.zfill(len("DD.MM.YYYY"))
   # Reverse DD.MM.YYYY into YYYY.MM.DD so that alphabetical order is the
   # same as chronological.
   date_parts = date.split(".")
-  date_parts.reverse()
+  if len(date_parts[0]) != 4:
+    date_parts.reverse()
   return ".".join(date_parts)
 
 def split_by_day(data, out_dir):
   # A dictionary from each date to a list of corresponding features
   daily_splits = {}
-  if TOPLEVEL_KEY in data:
-    for feature in data[TOPLEVEL_KEY]:
-      if "date_confirmation" in feature:
-        date = normalize_date(feature["date_confirmation"])
+  if "features" in data:
+    for feature in data["features"]:
+      if "properties" in feature and "date" in feature["properties"]:
+        date = normalize_date(feature["properties"]["date"])
         if date in daily_splits:
           daily_splits[date].append(feature)
         else:
@@ -42,14 +42,27 @@ def split_by_day(data, out_dir):
       f.write(json.dumps(daily_splits[date]))
       f.close()
 
+def convert_to_geojson(file_path):
+  functions.animation_formating_geo(file_path, TEMP_GEOJSON, 'day')
+
 def main():
+  if sys.version_info[0] < 3:
+    print("Sorry but I need Python 3 to work")
+    return
   if len(sys.argv) < 2:
     print("I need the path of the file to parse as an argument")
     return
-  in_file_path = sys.argv[1]
-  with open(in_file_path) as f:
+
+  # TODO: Support passinng data directly instead of writing to and
+  # re-reading from disk.
+  print("Converting to 'geojson' format...")
+  convert_to_geojson(sys.argv[1])
+  print("Splitting...")
+  with open(TEMP_GEOJSON) as f:
     split_by_day(json.loads(f.read()), ".")
     f.close()
+
+  os.remove(TEMP_GEOJSON)
 
 if __name__ == '__main__':
     main()
