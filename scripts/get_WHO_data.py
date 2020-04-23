4c75768f95ed107290b3d2f64a0f5e577a6e7b47
Manu Cornet <m@ma.nu>
2020-04-23 16:05:36 +0000

    WHO data scraping, remove a bunch of unnecessary URL params in the huge URL

--- scripts/get_WHO_data.py
+++ scripts/get_WHO_data.py
@@ -1,7 +1,7 @@
 '''
-Get WHO data for Global counts side bar on map. 
+Get WHO data for Global counts side bar on map.
 
-Query copied after using interface at : 
+Query generated and pruned after using interface at:
     https://services.arcgis.com/5T5nSi527N4F7luB/ArcGIS/rest/services/COVID_19_CasesByCountry(pl)_VIEW/FeatureServer/0/query
 '''
 
@@ -12,26 +12,26 @@ import os
 
 # Returns whether the operation was a success
 def get_WHO(out_path):
-    query = 'https://services.arcgis.com/5T5nSi527N4F7luB/ArcGIS/rest/services/COVID_19_CasesByCountry(pl)_VIEW/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=true&outFields=cum_conf%2C+ADM0_NAME&returnGeometry=false&returnCentroid=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token='
-    
+    query = 'https://services.arcgis.com/5T5nSi527N4F7luB/ArcGIS/rest/services/COVID_19_CasesByCountry(pl)_VIEW/FeatureServer/0/query?where=1%3D1&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&units=esriSRUnit_Meter&returnGeodetic=true&outFields=cum_conf%2C+ADM0_NAME&returnGeometry=false&returnCentroid=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&returnZ=false&returnM=false&returnExceededLimitFeatures=true&f=pjson'
+
     req = requests.get(query)
-    
+
     if req.status_code == 200:
         data = json.loads(req.text)
         features = {
-                'features': sorted(data['features'], 
-                    key=lambda x: int(x['attributes']['cum_conf']), 
+                'features': sorted(data['features'],
+                    key=lambda x: int(x['attributes']['cum_conf']),
                     reverse=True)
                 }
-    
+
         drop = None
         new    = 'legendGroup'
-        values = [10, 100, 500, 2000] 
+        values = [10, 100, 500, 2000]
         for ind,f in enumerate(features['features']):
             attributes = f['attributes']
             if attributes['ADM0_NAME'] == 'International conveyance (Diamond Princess)':
                 drop = ind
-    
+
             count = int(attributes['cum_conf'])
             if count <= 10:
                 attributes[new] = '10'
@@ -44,10 +44,10 @@ def get_WHO(out_path):
             else:
                 attributes[new] = 'default'
             attributes['cum_conf'] = '{:,}'.format(count)
-    
+
         if drop is not None:
             del(features['features'][drop])
-    
+
         with open(out_path, 'w') as F:
             json.dump(features, F)
     else:
