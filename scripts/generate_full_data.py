177e508c5b37f8ee9eeda44c0121d2977d7fd6de
tbrewer-healthmap <61703660+tbrewer-healthmap@users.noreply.github.com>
2020-04-27 18:20:00 -0400

    generate full data
    Revisited data generation.

--- scripts/generate_full_data.py
+++ scripts/generate_full_data.py
@@ -12,166 +12,245 @@ import sys
 from io import StringIO
 import re
 import json
+import os
+import split
+import multiprocessing 
+
 
 jhu_url= 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
 
+latestdata_url = 'https://raw.githubusercontent.com/beoutbreakprepared/nCoV2019/master/latest_data/latestdata.csv'
 
-# Columns we're keeping. 
-COLNAMES = ['ID', 'latitude', 'longitude',
-            'city', 'province', 'country',
-            'age', 'sex', 'symptoms', 
-            'source', 'date_confirmation', 
-            'geo_resolution']
+
 
 
 parser = argparse.ArgumentParser(description='Generate full-data.json file')
-parser.add_argument('-l', '--latest', type=str, required=True, 
-        help='path to latestdata.csv')
-parser.add_argument('-o', '--outfile', required=True, type=str,
-        help='path to output file')
-parser.add_argument('-j', '--jhu', type=str, help='Option to save jhu data (before formating)',
+
+parser.add_argument('out_dir', type=str, default = '.',
+        help='path to dailies directory')
+
+parser.add_argument('-l', '--latest', type=str, default=False, 
+        help='path to read latestdata.csv locally, fetch from github otherwise')
+
+parser.add_argument('-f', '--full', type=str, default=False,
+        help="option to save full-data")
+
+parser.add_argument('-j', '--jhu', type=str, 
+        help='Option to save jhu data (before formating)',
         default=False)
 
-def jhu(url, jhu_file):
+
+parser.add_argument('-t', '--timeit', action='store_const', const=True,
+        help='option to print execution time')
+
+parser.add_argument('--input_jhu', default='', type=str,
+        help='read from local jhu file')
+
+
+def prepare_latest_data(infile, **kwargs):
+    if infile : 
+        readfrom = infile
+    else:
+        req = requests.get(latestdata_url)
+        if req.status_code != 200:
+            print('could not get latestdata.csv, aborting')
+            sys.exit(1)
+        readfrom = StringIO(req.text)
+
+    df = pd.read_csv(readfrom,  
+            usecols=['country', 'date_confirmation', 'latitude', 'longitude'])
+    
+    roundto = kwargs.get('roundto', 4)
+    df['latitude'] = df.latitude.round(roundto).astype(str)
+    df['longitude'] = df.longitude.round(roundto).astype(str)
+
+    # filters 
+    df = df[~df.country.isin(['United States', 'Virgin Islands, U.S.', 'Puerto Rico'])]
+    df = df[~df.latitude.isnull() | df.longitude.isnull()] 
+    df = df[~df.latitude.str.contains('[aA-zZ]', regex=True)]
+    df = df[~df.longitude.str.contains('[aA-zZ]', regex=True)]
+    df['date_confirmation'] = df.date_confirmation.str.extract('(\d{2}\.\d{2}\.\d{4})')
+    df = df[pd.to_datetime(df.date_confirmation,
+        format="%d.%m.%Y", errors='coerce') < pd.datetime.now()]
+
+    
+    # geoids
+    df['geoid'] = df['latitude'] + '|' + df['longitude']
+    df = df.drop(['country', 'latitude', 'longitude'], axis=1)
+
+    dates  = df.date_confirmation.unique()
+    geoids = df.geoid.unique()
+    geoids.sort()
+    
+    new = pd.DataFrame(columns=geoids, index=dates)
+    new.index.name = 'date'
+    for i in new.index:
+        counts = df[df.date_confirmation == i].geoid.value_counts()
+        new.loc[i] = counts
+    new = new.fillna(0)
+    new.reset_index(drop=False)
+    return new
+
+def prepare_jhu_data(outfile, read_from_file, **kwargs):
     '''
     Get JHU data from URL and format to 
     to be compatible with full-data.json
     (used for US data)
     '''
 
-    # Get JHU data
-    req = requests.get(jhu_url)
-    if req.status_code != 200:
-        print('Could not get JHU data, aborting')
-        sys.exit(1)
-    
-    print(req.text[:100])
-    us_data = pd.read_csv(StringIO(req.text))
-    print(us_data) 
-    if jhu_file:
-        with open(jhu_file, 'w') as F:
-            F.write(req.text)
+    if read_from_file:
+        read_from = read_from_file
+    else:
+        # Get JHU data
+        req = requests.get(jhu_url)
+        if req.status_code != 200:
+            print('Could not get JHU data, aborting')
+            sys.exit(1)
+        read_from = StringIO(req.text)
 
-    # rename date columns to match sheet format (%d.%m.%d)
+    df = pd.read_csv(read_from) 
+
+    if outfile:
+        df.to_csv(outfile, index=False)
+
+    roundto = kwargs.get('roundto', 4)
+    df['Lat'] = df.Lat.round(roundto)
+    df['Long_'] = df.Long_.round(roundto)
+
+    # do some filtering
+    df = df.dropna()
+    df = df[df.Admin2 != 'Unassigned']
+    df = df[~((df.Lat == 0) & (df.Long_ == 0))]
+
+    df['geoid'] = df.Lat.astype(str) + '|' + df['Long_'].astype(str)
+    df.drop(['Lat', 'Long_'], axis=1, inplace=True)
+
     rx = '\d{1,2}/\d{1,2}/\d'
-    date_columns = []
-
-    for c in us_data.columns:
-        print(c)
-        if re.match(rx, c):
-            month, day, year = c.split('/')
-            month = str(month).zfill(2)
-            day   = str(day).zfill(2)
-            year = '20' + str(year) if len(year) == 2 else year
-            date = '{}.{}.{}'.format(day, month, year)
-            date_columns.append(date)
-            us_data.rename({c : date}, axis=1, inplace=True)
-            print(date)
-    date_columns = sorted(date_columns, key=lambda x: pd.to_datetime(x, format='%d.%m.%Y'))
+    date_columns = [c for c in df.columns if re.match(rx, c)]
+    keep = ['geoid'] + date_columns
+    df = df[keep]
 
 
-    # reformat JHU to have same structure as sheet
-    # This may seem silly because we unpack and then reduce to Unique again,
-    # we do it because the "full_data" file is sent to FM-Global
+    # rename to match latestdata format
+    new_dates = []
+    for c in date_columns:
+        month, day, year = c.split('/')
+        month = month.zfill(2)
+        day = day.zfill(2)
+        year = '20' + year if len(year) == 2 else year
+        new = f'{day}.{month}.{year}'
+        new_dates.append(new)
+    df.rename(dict(zip(date_columns, new_dates)), axis=1, inplace=True)
+    
+    df = df.set_index('geoid') 
+    df = df - df.shift(1, axis=1).fillna(0).astype(int)
+    
+    # some entries are inconsistent, i.e. not really cumulative for those
+    # we assign a value of zero (for new cases).  Induces a bit of error, but 
+    # preferable than ignoring entirely. 
+    df[df < 0] = 0
 
-    # only defining columns where values will change for now.
-    # Loop is done in this order so that IDs persist.
-    print(len(us_data[us_data.Lat.astype(float) == 0]))
-    us_reformed = pd.DataFrame(columns=['latitude', 'longitude', 'city', 'province', 'date_confirmation'])
-    for i, date in enumerate(date_columns):
-        print(date)
-        for j, row in us_data.iterrows():
-            if float(row['Lat']) == 0 or float(row['Long_']) == 0:
-                continue
+    df = df.T
+    df.index.name = 'date'
+    df.reset_index(inplace=True) 
+    return df 
 
-            if pd.isnull(row['Lat']) or pd.isnull(row['Long_']):
-                continue
 
-            if i == 0:
-                N = int(row[date])
-            else:
-                N = int(row[date]) - int(row[date_columns[i-1]])
 
-                entry = {
-                    'latitude': row['Lat'],
-                    'longitude': row['Long_'],
-                    'city': row['Admin2'],
-                    'province': row['Province_State'],
-                    'date_confirmation': date
+def daily_slice(new_cases, total_cases):
+    # full starts from new cases by location/date
+    # structure for daily slice YYYY.MM.DD.json
+    #{"date": "YYYY-MM-DD", "features": [{"properties": {"geoid": "lat|long",
+    # "new": int, "total": int}}, ... ]
+    
+    assert new_cases.name == total_cases.name, "mismatched dates"
+    
+    date = new_cases.name
+    daily_dict = {"date": split.normalize_date(date).replace('.', '-'),
+                "features": []}
+
+    for i in new_cases.index:
+        new = new_cases[i]
+        total = total_cases[i]
+        if new == total == 0:
+            continue
+        else:
+            entry = {
+                "properties" : {
+                    "geoid": i,
+                    "new": int(new),
+                    "total": int(total)
                 }
-                us_reformed = us_reformed.append([entry]*N, ignore_index=True)
+            }
+            daily_dict['features'].append(entry)
+  
+    return daily_dict
 
-    us_reformed.insert(0, 'ID', range(1, len(us_reformed) + 1))
-    us_reformed['ID'] = us_reformed['ID'].apply(lambda x : 'JHU' + str(x))
-    us_reformed.insert(5, 'country', 'United States')
-    us_reformed.insert(6, 'age', '')
-    us_reformed.insert(7, 'sex', '')
-    us_reformed.insert(8, 'symptoms', '')
-    us_reformed.insert(9, 'source', 'JHU')
-    us_reformed.insert(11, 'geo_resolution', 'admin2')
-
-
-    return us_reformed 
-
-def latestdata(path): 
+def chunks(new_cases, total_cases):
     '''
-    Read latestdata.csv and filter out US/Virgin Isllands/ Puerto Rico
-    These are in JHU data. 
-    '''    
-    df = pd.read_csv(path, dtype=str)
-    filter_ = ~df.country.isin(['United States', 'Virgin Islands, U.S.', 'Puerto Rico'])
-    return df[filter_]
-
-
-def clean_data(data: pd.DataFrame, colnames: list) -> pd.DataFrame:
+    Yields successive equal-sized chunks from the input list.
     '''
-    Basic cleaning and filtering:
-    - valid lat/longs
-    - valid dates (using %d.%m.%Y format)
-    - manage white space
-    - Keeps only columns that are going to be in final version.
+    for i in range(len(new_cases)):
+        yield (new_cases.iloc[i], total_cases.iloc[i])
+        
 
-    Args:
-    :data: pd.DataFrame, data from sheet
-    :colnames: list, list of columns we are keeping for final version
-    '''
-    df = data.copy()
-    df.rename({x: x.strip() for x in df.columns}, inplace=True, axis=1)
-
-    # drop invalid lat/longs
-    lat,lon     = df.latitude, df.longitude
-    invalid_lat = lat.str.contains('#REF') | lat.str.contains('N/A') | lat.isnull() | (lat == '')
-    invalid_lon = lon.str.contains('#REF') | lon.str.contains('N/A') | lon.isnull() | (lon == '')
-    invalid     = invalid_lat | invalid_lon
-    df = df[~invalid] # NOT invalid
-
-    # Only keep those that have a date_confirmation
-    df['date_confirmation'] = df['date_confirmation'].str.strip() # some have empty spaces
-    dc = df.date_confirmation
-    valid_date = (dc != '') & ~(dc.isnull())
-    df = df[valid_date]
-
-    # Basic cleaning for strings
-    for c in ['city', 'province', 'country']:
-        df[c] = df[c].str.strip()
-        df[c] = df[c].str.title()
-        df[c] = df[c].str.replace('\xa0', ' ') # encoding for a space that was found in some entries.
-
-    # Only keep the columns we want
-    df = df[colnames]
-
-    return df
 
 
 def main():
-    args = parser.parse_args()
-    jhu_data = jhu(jhu_url, jhu_file=args.jhu)
-    latest   = latestdata(args.latest)
-    data = latest.append(jhu_data, ignore_index=True)
-    data = clean_data(data, COLNAMES)
-    data = {'data': data.to_dict(orient='records')}
-    with open(args.outfile, 'w') as F:
-        json.dump(data, F)
+  args = parser.parse_args()
+    
+  if args.timeit:
+      import time
+      t0 = time.time()
+
+  latest = prepare_latest_data(args.latest) 
+  jhu = prepare_jhu_data(args.jhu, args.input_jhu)
+  full = latest.merge(jhu, on='date', how='outer')
+  full.fillna(0, inplace=True)
+  full = full.set_index('date')
+  print(full.index[:10])
+  print(full.columns[:10])
+  for c in full.columns:
+      if c == 'date': 
+          continue
+      else : 
+          full[c] = full[c].astype(int)
+  
+  # drop columns with negative values (errors in JHU data)
+  # Hopefully theny will be fixed at some point. 
+  if args.full:
+      full.to_csv(args.full)
+    
+  latest_date = split.normalize_date(full.index[-1]).replace('.', '-')
+
+  new_cases = full
+  total_cases = new_cases.cumsum()
+    
+  n_cpus = multiprocessing.cpu_count()
+  print("Processing " + str(len(full)) + " features "
+          "with " + str(n_cpus) + " threads...")
+
+  pool = multiprocessing.Pool(n_cpus)
+  out_slices = pool.starmap(daily_slice, chunks(new_cases, total_cases), chunksize=10)
+
+
+  for s in out_slices:
+    out_name = ("latest" if s['date'] == latest_date else split.normalize_date(s['date'])) + '.json'
+    daily_slice_file_path = os.path.join(args.out_dir, out_name) 
+
+    if os.path.exists(daily_slice_file_path):
+        print("I will not clobber '" + daily_slice_file_path + "', " "please delete it first")
+        continue
+
+    with open(daily_slice_file_path, "w") as f:
+        f.write(json.dumps(s))
+
+
+
+  if args.timeit:
+      print(round(time.time(), 2) - t0, "seconds")
+
+
 
 if __name__ == '__main__':
     main()
