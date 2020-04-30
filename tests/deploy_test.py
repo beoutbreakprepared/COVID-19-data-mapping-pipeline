2bacfe717b9f70c3d74e5ef1613a6490b3ca5392
Manu Cornet <m@ma.nu>
2020-04-30 15:29:37 +0000

    Modularize the 'run' logic to make it easier to reuse and test

--- tests/deploy_test.py
+++ tests/deploy_test.py
@@ -1,8 +1,12 @@
 import base_test
+import sys
+
+sys.path.append("scripts")
+import deploy
 
 class DeployTest(base_test.BaseTest):
-  def display_name(self):
-    return "Deployment tests"
+    def display_name(self):
+        return "Deployment tests"
 
-  def run(self):
-    return False
+    def run(self):
+        return False
