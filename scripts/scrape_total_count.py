165f84a16276c197a8e6ae4af4ec6c4d3b99e481
Thomas Brewer <thomas.brewer@childrens.harvard.edu>
2020-05-29 16:24:53 -0400

    change latest counts source

--- scripts/scrape_total_count.py
+++ scripts/scrape_total_count.py
@@ -1,29 +1,21 @@
 import json
 import os
-import urllib.request
-import sys
-from bs4 import BeautifulSoup
 from datetime import datetime
+import pandas as pd
 
 # Returns whether the operation was successful
 def scrape_total_count(out_path):
-
-    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR30F8lYP3jG7YOq8es0PBpJIE5yvRVZffOyaqC0GgMBN6yt0Q-NI8pxS7hd1F9dYXnowSC6zpZmW9D/pubhtml/sheet?headers=false&gid=0&range=A1:I183"
-    fp = urllib.request.urlopen(url)
-    mybytes = fp.read()
-
-    html = mybytes.decode("utf8")
-    fp.close()
-
-    soup = BeautifulSoup(html, "html.parser")
-    try:
-        count = soup.select_one("tbody tr:nth-of-type(5) td:nth-of-type(1)").text
-    except NotImplementedError as e:
-        print("I wasn't able to scrape the number I wanted: " + str(e))
+    url = "http://opendata.ecdc.europa.eu/covid19/casedistribution/csv/"
+    try: 
+        data = pd.read_csv(url, usecols=['cases'])
+    except:
+        print("I wasn't able to fetch the file I wanted: " + str(url))
         return False
 
+    count = data['cases'].sum()
+
     date = datetime.now().strftime("%Y-%m-%d")
-    results = {"caseCount": count, "date": date}
+    results = {"caseCount": int(count), "date": date}
 
     with open(out_path, "w") as F:
         json.dump([results], F)
