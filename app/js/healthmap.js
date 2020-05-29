e9a7ee5b37906a746b1a05aa661e513b5a42fa35
Manu Cornet <m@ma.nu>
2020-05-29 23:58:50 +0000

    Add a simple 'autodrive' mode where interactive elements are hidden

--- app/js/healthmap.js
+++ app/js/healthmap.js
@@ -565,6 +565,12 @@ function loadCountryData() {
 
 
 function initMap() {
+
+  const hash = window.location.href.split('#')[1] || '';
+  if (hash == 'autodrive') {
+    document.body.classList.add('autodrive');
+  }
+
   mapboxgl.accessToken = MAPBOX_TOKEN;
   map = new mapboxgl.Map({
     'container': 'map',
